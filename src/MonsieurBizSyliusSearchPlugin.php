7ca33ea90e7d923844822d681ee81bc4fe0f1613
Maxime Huran <m.huran@monsieurbiz.com>
2019-10-20 22:45:33 +0200

    Add plugin skeleton

---
+++ src/MonsieurBizSyliusSearchPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusSearchPLugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class MonsieurBizSyliusSearchPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}

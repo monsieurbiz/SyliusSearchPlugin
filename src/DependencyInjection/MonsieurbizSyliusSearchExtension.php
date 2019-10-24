e50d4d46a42beefadffe9f38b24f1bad9eec23f6
Maxime Huran <m.huran@monsieurbiz.com>
2019-10-24 15:23:34 +0200

    Remove non existing services.xml

--- src/DependencyInjection/MonsieurbizSyliusSearchExtension.php
+++ src/DependencyInjection/MonsieurbizSyliusSearchExtension.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Monsieurbiz\SyliusSearchPlugin\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 final class MonsieurbizSyliusSearchExtension extends Extension
 {
@@ -17,8 +15,5 @@ final class MonsieurbizSyliusSearchExtension extends Extension
     public function load(array $config, ContainerBuilder $container): void
     {
         $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-
-        $loader->load('services.xml');
     }
 }

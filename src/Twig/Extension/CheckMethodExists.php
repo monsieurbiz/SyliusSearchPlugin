cc43e6bb8063e72a5690ca57273938cf572df971
Maxime Huran <m.huran@monsieurbiz.com>
2020-05-20 08:55:23 +0200

    Keep compatibilty with versions older than 1.7 in templates

---
+++ src/Twig/Extension/CheckMethodExists.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusSearchPlugin\Twig\Extension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class CheckMethodExists extends AbstractExtension
+{
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            new TwigFunction('bundle_exists', array($this, 'bundleExists')),
+        );
+    }
+
+    public function bundleExists($bundle){
+        return array_key_exists(
+            $bundle,
+            $this->container->getParameter('kernel.bundles')
+        );
+    }
+}

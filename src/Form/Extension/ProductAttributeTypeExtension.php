5cdfe1ec69f6c292d27674d9212d158160957700
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2020-06-18 15:16:03 +0200

    Add filterable field to product options/attributes and update admin

---
+++ src/Form/Extension/ProductAttributeTypeExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusSearchPlugin\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Sylius\Bundle\ProductBundle\Form\Type\ProductAttributeType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class ProductAttributeTypeExtension extends AbstractTypeExtension
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('filterable', CheckboxType::class, [
+                'label' => 'monsieurbiz_searchplugin.admin.product_attribute.form.filterable',
+                'required' => true,
+            ])
+        ;
+    }
+
+    public function getExtendedTypes(): array
+    {
+        return [
+            ProductAttributeType::class,
+        ];
+    }
+}
---
+++ src/Form/Extension/ProductOptionTypeExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusSearchPlugin\Form\Extension;
+
+use Sylius\Bundle\ProductBundle\Form\Type\ProductOptionType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class ProductOptionTypeExtension extends AbstractTypeExtension
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('filterable', CheckboxType::class, [
+                'label' => 'monsieurbiz_searchplugin.admin.product_option.form.filterable',
+                'required' => true,
+            ])
+        ;
+    }
+
+    public function getExtendedTypes(): array
+    {
+        return [
+            ProductOptionType::class,
+        ];
+    }
+}

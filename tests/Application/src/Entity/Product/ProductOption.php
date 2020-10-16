36c10bdb6ef3ea447fb5cf829e8de451076146b6
Maxime Huran <m.huran@monsieurbiz.com>
2020-10-16 14:18:12 +0200

    Override product option and product attribute on test application

---
+++ tests/Application/src/Entity/Product/ProductOption.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\MonsieurBiz\SyliusSearchPlugin\App\Entity\Product;
+
+use Doctrine\ORM\Mapping as ORM;
+use MonsieurBiz\SyliusSearchPlugin\Entity\Product\FilterableInterface;
+use MonsieurBiz\SyliusSearchPlugin\Model\Product\FilterableTrait;
+use Sylius\Component\Product\Model\ProductOption as BaseProductOption;
+use Sylius\Component\Product\Model\ProductOptionTranslation;
+use Sylius\Component\Product\Model\ProductOptionTranslationInterface;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="sylius_product_option")
+ */
+class ProductOption extends BaseProductOption implements FilterableInterface
+{
+    use FilterableTrait;
+
+    protected function createTranslation(): ProductOptionTranslationInterface
+    {
+        return new ProductOptionTranslation();
+    }
+}

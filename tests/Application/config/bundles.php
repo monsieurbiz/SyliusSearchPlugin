d3e1b839de1418c2e970f770f25d83bac5747159
Maxime Huran <m.huran@monsieurbiz.com>
2021-05-24 17:04:11 +0200

    Add new dynamic test application with infra

---
+++ tests/Application/config/bundles.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Search plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+return array_merge(
+    require dirname(__DIR__) . '/../../vendor/sylius/sylius/config/bundles.php',
+    [
+        MonsieurBiz\SyliusSearchPlugin\MonsieurBizSyliusSearchPlugin::class => ['all' => true],
+    ]
+);

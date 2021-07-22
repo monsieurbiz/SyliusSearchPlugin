102138b0e2e1c3b34e413da5648347172fe6bb41
surrok <surrok6@gmail.com>
2021-07-22 08:33:15 +0300

    Fix retrieving all attributes aggs

--- src/Helper/AggregationHelper.php
+++ src/Helper/AggregationHelper.php
@@ -39,7 +39,7 @@ class AggregationHelper
             ],
             'aggs' => [
                 'values' => [
-                    'terms' => ['field' => 'attributes.value.keyword'],
+                    'terms' => ['field' => 'attributes.value.keyword', self::MAX_AGGREGATED_ATTRIBUTES_INFO],
                 ],
             ],
         ];

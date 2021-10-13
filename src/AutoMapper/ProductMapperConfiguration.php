0cae500e1b04ae4185b4256f1efbd02904404031
Maxime Leclercq <m.leclercq@monsieurbiz.com>
2021-10-13 15:47:57 +0200

    refactor: replace ProductDTO by a eater model with mapper

--- src/AutoMapper/ProductMapperConfiguration.php
+++ src/AutoMapper/ProductMapperConfiguration.php
@@ -15,22 +15,27 @@ namespace MonsieurBiz\SyliusSearchPlugin\AutoMapper;
 
 use App\Entity\Product\Product;
 use Jane\Bundle\AutoMapperBundle\Configuration\MapperConfigurationInterface;
+use Jane\Component\AutoMapper\AutoMapperInterface;
 use Jane\Component\AutoMapper\MapperGeneratorMetadataInterface;
 use Jane\Component\AutoMapper\MapperMetadata;
 use MonsieurBiz\SyliusSearchPlugin\Entity\Product\SearchableInterface;
-use MonsieurBiz\SyliusSearchPlugin\Event\ProductMapperConfigurationEvent;
-use MonsieurBiz\SyliusSearchPlugin\Generated\Model\Product as ProductDTO;
+use MonsieurBiz\SyliusSearchPlugin\Generated\Model\Channel;
+use MonsieurBiz\SyliusSearchPlugin\Generated\Model\Image;
 use MonsieurBiz\SyliusSearchPlugin\Generated\Model\ProductAttribute;
+use MonsieurBiz\SyliusSearchPlugin\Generated\Model\ProductTaxon;
+use MonsieurBiz\SyliusSearchPlugin\Generated\Model\Taxon;
+use MonsieurBiz\SyliusSearchPlugin\Model\Product\ProductDTO;
+use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Core\Model\ProductInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Sylius\Component\Core\Model\ProductTaxonInterface;
 
-class ProductMapperConfiguration implements MapperConfigurationInterface
+final class ProductMapperConfiguration implements MapperConfigurationInterface
 {
-    private EventDispatcherInterface $eventDispatcher;
+    private AutoMapperInterface $autoMapper;
 
-    public function __construct(EventDispatcherInterface $eventDispatcher)
+    public function __construct(AutoMapperInterface $autoMapper)
     {
-        $this->eventDispatcher = $eventDispatcher;
+        $this->autoMapper = $autoMapper;
     }
 
     public function process(MapperGeneratorMetadataInterface $metadata): void
@@ -39,7 +44,56 @@ class ProductMapperConfiguration implements MapperConfigurationInterface
             return;
         }
 
-        $metadata->forMember('attributes', function(ProductInterface $product) {
+        $metadata->forMember('id', function(ProductInterface $product): int {
+            return $product->getId();
+        });
+
+        $metadata->forMember('code', function(ProductInterface $product): ?string {
+            return $product->getCode();
+        });
+
+        $metadata->forMember('enabled', function(ProductInterface $product): bool {
+            return $product->isEnabled();
+        });
+
+        $metadata->forMember('slug', function(ProductInterface $product): ?string {
+            return $product->getSlug();
+        });
+
+        $metadata->forMember('name', function(ProductInterface $product): ?string {
+            return $product->getName();
+        });
+
+        $metadata->forMember('description', function(ProductInterface $product): ?string {
+            return $product->getDescription();
+        });
+
+        $metadata->forMember('images', function(ProductInterface $product): array {
+            $images = [];
+            foreach ($product->getImages() as $image) {
+                $images[] = $this->autoMapper->map($image, Image::class);
+            }
+
+            return $images;
+        });
+
+        $metadata->forMember('mainTaxon', function(ProductInterface $product): Taxon {
+            return $this->autoMapper->map($product->getMainTaxon(), Taxon::class);
+        });
+
+        $metadata->forMember('product_taxons', function(ProductInterface $product): array {
+            return array_map(function(ProductTaxonInterface $productTaxon) {
+                return $this->autoMapper->map($productTaxon, ProductTaxon::class);
+            }, $product->getProductTaxons()->toArray());
+        });
+
+        $metadata->forMember('channels', function(ProductInterface $product): array {
+            return array_map(function(ChannelInterface $channel) {
+                return $this->autoMapper->map($channel, Channel::class);
+            }, $product->getChannels()->toArray());
+        });
+
+        $metadata->forMember('attributes', function(ProductInterface $product): array {
             $attributes = [];
             /** @var \Sylius\Component\Product\Model\ProductAttributeValue $attributeValue */
             foreach ($product->getAttributes() as $attributeValue) {
@@ -47,22 +101,17 @@ class ProductMapperConfiguration implements MapperConfigurationInterface
                     continue;
                 }
                 $attribute = $attributeValue->getAttribute();
-                if ($attribute instanceof SearchableInterface && !$attribute->isSearchable() && !$attribute->isFilterable()) {
+                if (!$attribute instanceof SearchableInterface || (!$attribute->isSearchable() && !$attribute->isFilterable())) {
                     continue;
                 }
                 $attributeDTO = new ProductAttribute();
                 $attributeDTO->setName($attributeValue->getName());
                 $attributeDTO->setValue($attributeValue->getValue());
-                $attributes[$attributeValue->getCode()] = $attributeDTO;
+                $attributes[$attributeValue->getCode()] = $attributeDTO; // we can't use the automapper because value has a mixed type
             }
 
             return $attributes;
         });
-
-        $this->eventDispatcher->dispatch(
-            new ProductMapperConfigurationEvent($metadata),
-            ProductMapperConfigurationEvent::EVENT_NAME
-        );
     }
 
     public function getSource(): string
---
+++ src/Normalizer/Product/ProductDTONormalizer.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Search plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusSearchPlugin\Normalizer\Product;
+
+use MonsieurBiz\SyliusSearchPlugin\Generated\Model\Channel;
+use MonsieurBiz\SyliusSearchPlugin\Generated\Model\Image;
+use MonsieurBiz\SyliusSearchPlugin\Generated\Model\ProductAttribute;
+use MonsieurBiz\SyliusSearchPlugin\Generated\Model\ProductTaxon;
+use MonsieurBiz\SyliusSearchPlugin\Generated\Model\Taxon;
+use MonsieurBiz\SyliusSearchPlugin\Model\Product\ProductDTO;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+final class ProductDTONormalizer extends ObjectNormalizer implements DenormalizerInterface, NormalizerInterface, DenormalizerAwareInterface, NormalizerAwareInterface
+{
+    use DenormalizerAwareTrait;
+    use NormalizerAwareTrait;
+
+    public function denormalize($data, string $type, string $format = null, array $context = [])
+    {
+        /** @var ProductDTO $object */
+        $object = parent::denormalize($data, $type, $format, $context);
+
+        if (\array_key_exists('main_taxon', $data)) {
+            $object->setMainTaxon($this->denormalizer->denormalize($data['main_taxon'], Taxon::class, 'json', $context));
+            unset($data['main_taxon']);
+        }
+
+        if (\array_key_exists('product_taxons', $data)) {
+            $values = [];
+            foreach ($data['product_taxons'] as $value) {
+                $values[] = $this->denormalizer->denormalize($value, ProductTaxon::class, 'json', $context);
+            }
+            $object->setProductTaxons($values);
+            unset($data['product_taxons']);
+        }
+
+        if (\array_key_exists('images', $data) && null !== $data['images']) {
+            $values = [];
+            foreach ($data['images'] as $value) {
+                $values[] = $this->denormalizer->denormalize($value, Image::class, 'json', $context);
+            }
+            $object->setImages($values);
+            unset($data['product_taxons']);
+        }
+
+        if (\array_key_exists('channels', $data)) {
+            $values = [];
+            foreach ($data['channels'] as $value) {
+                $values[] = $this->denormalizer->denormalize($value, Channel::class, 'json', $context);
+            }
+            $object->setChannels($values);
+            unset($data['channels']);
+        }
+
+        if (\array_key_exists('attributes', $data)) {
+            $values = [];
+            foreach ($data['attributes'] as $key => $value) {
+                $values[$key] = $this->denormalizer->denormalize($value, ProductAttribute::class, 'json', $context);
+            }
+            $object->setAttributes($values);
+            unset($data['channels']);
+        }
+
+        return $object;
+    }
+
+    public function supportsDenormalization($data, string $type, string $format = null)
+    {
+        return ProductDTO::class === $type;
+    }
+
+    public function supportsNormalization($data, string $format = null)
+    {
+        return false;
+    }
+}

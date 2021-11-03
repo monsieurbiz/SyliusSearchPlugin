4cfcf17ad78d1478d5034bb423747d78c5bc4f1c
Maxime Huran <m.huran@monsieurbiz.com>
2021-11-03 18:51:38 +0100

    Add dist files

--- dist/src/Entity/Product/ProductAttribute.php
+++ dist/src/Entity/Product/ProductAttribute.php
@@ -11,14 +11,13 @@
 
 declare(strict_types=1);
 
-namespace Tests\MonsieurBiz\SyliusSearchPlugin\App\Entity\Product;
+namespace App\Entity\Product;
 
 use Doctrine\ORM\Mapping as ORM;
 use MonsieurBiz\SyliusSearchPlugin\Entity\Product\FilterableInterface;
 use MonsieurBiz\SyliusSearchPlugin\Model\Product\FilterableTrait;
 use Sylius\Component\Attribute\Model\AttributeTranslationInterface;
 use Sylius\Component\Product\Model\ProductAttribute as BaseProductAttribute;
-use Sylius\Component\Product\Model\ProductAttributeTranslation;
 
 /**
  * @ORM\Entity
--- dist/src/Entity/Product/ProductOption.php
+++ dist/src/Entity/Product/ProductOption.php
@@ -11,13 +11,12 @@
 
 declare(strict_types=1);
 
-namespace Tests\MonsieurBiz\SyliusSearchPlugin\App\Entity\Product;
+namespace App\Entity\Product;
 
 use Doctrine\ORM\Mapping as ORM;
 use MonsieurBiz\SyliusSearchPlugin\Entity\Product\FilterableInterface;
 use MonsieurBiz\SyliusSearchPlugin\Model\Product\FilterableTrait;
 use Sylius\Component\Product\Model\ProductOption as BaseProductOption;
-use Sylius\Component\Product\Model\ProductOptionTranslation;
 use Sylius\Component\Product\Model\ProductOptionTranslationInterface;
 
 /**

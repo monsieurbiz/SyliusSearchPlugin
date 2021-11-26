3d1938d74b17923d63f4c650dfd8dff65e859584
Maxime Leclercq <m.leclercq@monsieurbiz.com>
2021-11-26 16:29:20 +0100

    fix: search command

--- src/Command/SearchCommand.php
+++ src/Command/SearchCommand.php
@@ -14,33 +14,43 @@ declare(strict_types=1);
 namespace MonsieurBiz\SyliusSearchPlugin\Command;
 
 use MonsieurBiz\SyliusSearchPlugin\Model\Product\ProductDTO;
+use MonsieurBiz\SyliusSearchPlugin\Search\Request\RequestConfiguration;
 use MonsieurBiz\SyliusSearchPlugin\Search\RequestFactory;
 use MonsieurBiz\SyliusSearchPlugin\Search\RequestInterface;
 use MonsieurBiz\SyliusSearchPlugin\Search\Search;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class SearchCommand extends Command
 {
     protected static $defaultName = 'monsieurbiz:search:search';
     private RequestFactory $requestFactory;
     private Search $search;
+    private RequestStack $requestStack;
 
     public function __construct(
-        RequestFactory $requestFactory, Search $search, $name = null
+        RequestFactory $requestFactory,
+        Search $search,
+        RequestStack $requestStack,
+        $name = null
     ) {
         parent::__construct($name);
         $this->requestFactory = $requestFactory;
         $this->search = $search;
+        $this->requestStack = $requestStack;
     }
 
     protected function configure(): void
     {
         parent::configure();
         $this->addArgument('query', InputArgument::REQUIRED, 'Search query');
+        $this->addOption('channel', 'c', InputOption::VALUE_OPTIONAL, 'Channel code', 'FASHION_WEB');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -48,10 +58,15 @@ class SearchCommand extends Command
         $io = new SymfonyStyle($input, $output);
 
         $query = $input->getArgument('query');
-        $request = $this->requestFactory->create(RequestInterface::SEARCH_TYPE, 'monsieurbiz_product');
-        $request->setQueryParameters(['query_text' => $query]);
 
-        $result = $this->search->query($request);
+        $request = new Request(['query' => $query, '_channel_code' => $input->getOption('channel')]);
+        $this->requestStack->push($request);
+
+        $requestConfiguration = new RequestConfiguration($request);
+        $elasticsearchRequest = $this->requestFactory->create(RequestInterface::SEARCH_TYPE, 'monsieurbiz_product');
+        $elasticsearchRequest->setConfiguration($requestConfiguration);
+
+        $result = $this->search->query($requestConfiguration, $elasticsearchRequest);
         $io->title('Search result for: ' . $query);
         $io->section('Nb results: ' . $result->count());
         $documents = [];

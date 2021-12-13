51757024611606a2b23b337370fbfc7f8ac4f1ce
Maxime Leclercq <m.leclercq@monsieurbiz.com>
2021-12-13 10:11:03 +0100

    feat: change input search type

--- src/Form/Type/SearchType.php
+++ src/Form/Type/SearchType.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace MonsieurBiz\SyliusSearchPlugin\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType as SymfonySearchType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -29,7 +30,7 @@ class SearchType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('query', TextType::class, [
+            ->add('query', SymfonySearchType::class, [
                 'required' => true,
                 'label' => 'monsieurbiz_searchplugin.form.query',
                 'attr' => [

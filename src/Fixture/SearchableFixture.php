bc4c8b5f4adc2297e762c7e0f4570f499ca823a8
Maxime Huran <m.huran@monsieurbiz.com>
2022-02-02 14:34:01 +0100

    Add Searchable fixtures and factory

---
+++ src/Fixture/SearchableFixture.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Search plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusSearchPlugin\Fixture;
+
+use Doctrine\ORM\EntityManagerInterface;
+use MonsieurBiz\SyliusSearchPlugin\Fixture\Factory\SearchableFixtureFactoryInterface;
+use Sylius\Bundle\CoreBundle\Fixture\AbstractResourceFixture;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+class SearchableFixture extends AbstractResourceFixture implements SearchableFixtureInterface
+{
+    /**
+     * SearchableFixture constructor.
+     */
+    public function __construct(
+        EntityManagerInterface $productManager,
+        SearchableFixtureFactoryInterface $exampleFactory
+    ) {
+        parent::__construct($productManager, $exampleFactory);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getName(): string
+    {
+        return 'monsieurbiz_sylius_search';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function configureResourceNode(ArrayNodeDefinition $resourceNode): void
+    {
+        $resourceNode
+            ->children()
+            ->scalarNode('attribute')->end()
+            ->scalarNode('option')->end()
+            ->booleanNode('filterable')->defaultValue(true)->end()
+            ->booleanNode('searchable')->defaultValue(true)->end()
+        ;
+    }
+}

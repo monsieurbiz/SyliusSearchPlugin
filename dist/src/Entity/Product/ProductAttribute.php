ee94165226e354299e18aac8fec4cfef90c14361
Maxime Huran <m.huran@monsieurbiz.com>
2024-07-13 15:49:38 +0200

    Use PHP8 attribute in dist file override

--- dist/src/Entity/Product/ProductAttribute.php
+++ dist/src/Entity/Product/ProductAttribute.php
@@ -23,6 +23,8 @@ use Sylius\Component\Product\Model\ProductAttribute as BaseProductAttribute;
  * @ORM\Entity
  * @ORM\Table(name="sylius_product_attribute")
  */
+#[ORM\Entity]
+#[ORM\Table(name: 'sylius_product_attribute')]
 class ProductAttribute extends BaseProductAttribute implements SearchableInterface
 {
     use SearchableTrait;
